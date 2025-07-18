e5b22bcf04ba57a7f8050c1fecf02458cf62c17d
ayltonkaua <iayltonkaua001@gmail.com>
2025-07-17 22:48:25 -0300

    correção de erro depoy

--- supabase.js
+++ supabase.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 // Inicializa o cliente do Supabase
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_KEY
+  process.env.SUPABASE_ANON_KEY
 );
 
 /**
